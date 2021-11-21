4ba59c9a787e8fa5a83d2453e5a7700b93a8d8ea
Hongchen WANG <hongchen.wang@hotmail.fr>
2021-11-21 20:00:10 +0100

    comment and mongoClient option for server test

--- api/restaurants.route.js
+++ api/restaurants.route.js
@@ -2,6 +2,6 @@ import express from "express"
 
 const router = express.Router()
 
-router.route("/").get((req, res) => res.send("hello world"))
+router.route("/").get((req, res) => res.send("TEST URL HELLO WORLD"))
 
 export default router
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./server.js"
 import mongodb from "mongodb"
-import dotnet from "dotenv"
+import dotenv from "dotenv"
 dotenv.config()
 const MongoClient = mongodb.MongoClient
 
@@ -9,16 +9,16 @@ const port = process.env.PORT || 8000
 MongoClient.connect(
     process.env.RESTREVIEWS_DB_URI,
     {
-        poolSize: 50, 
-        wtimeout: 250,
-        useNewUrlParse: true
-    }
-)
+        maxPoolSize: 50, 
+        wtimeoutMS: 2500,
+        useNewUrlParser: true
+})
 .catch(err => {
     console.error(err.stack)
     process.exit(1)
 })
 .then(async client => {
+    // start web server 
     app.listen(port, () => {
         console.log(`listening on prot ${port}`)
     })
