e915650388df4241364c84fc18c56296ec54a674
Hongchen WANG <hongchen.wang@hotmail.fr>
2022-02-13 18:26:30 +0100

    Following restaurants-list and http-common using axios

--- frontend/src/components/restaurants.js
+++ frontend/src/components/restaurants.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useEffect, useState } from "react";
+import RestaurantDataService from "../services/restaurant.js";
+import {Link} from "react-router-dom";
 
 function Restaurants() {
   return (
